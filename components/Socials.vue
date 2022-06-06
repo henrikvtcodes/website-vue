11dd39469476ccb8d6d91325d47b6c11a02a40f3
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-06-06 10:19:23 -0400

    check in social icons component

---
+++ components/Socials.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="mt-2 mb-2 flex space-x-2 md:space-x-4">
+    <span class=" hover:cursor-pointer hover:text-black">
+      <NuxtLink to="https://github.com/henrikvtcodes" class="no-underline" target="_blank">
+        <Github class="sm:w-9 w-6 h-auto" />
+      </NuxtLink>
+    </span>
+    <span class=" hover:cursor-pointer hover:text-blue-twitter ">
+      <NuxtLink to="https://twitter.com/henrik_tech" class="no-underline" target="_blank">
+        <Twitter class="sm:w-9 w-6 h-auto" />
+      </NuxtLink>
+    </span>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Github, Twitter } from 'lucide-vue-next';
+
+export default {
+  name: 'Socials',
+  components: {
+    Github,
+    Twitter,
+  },
+}
+
+</script>
