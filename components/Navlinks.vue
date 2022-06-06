c760fa8697ff39f54cbdc40d465cfe27d2d46de4
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-06-06 09:53:29 -0400

    navlinks component

---
+++ components/Navlinks.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav className="w-full flex sm:flex-row flex-col justify-center content-center my-4 sm:gap-x-2 gap-y-2">
+    <NuxtLink to="https://blog.henriktech.com" class="no-underline basis-1/2" target="_blank">
+      <div
+        class="w-full py-2 text-center align-middle font-semibold text-sm text-white rounded bg-white bg-opacity-10 hover:bg-opacity-20">
+        Blog
+      </div>
+    </NuxtLink>
+    <NuxtLink to="https://consulting.henrikvt.com?utm_campaign=links&utm_source=portfolio&utm_medium=web"
+      class="no-underline basis-1/2" target="_blank">
+      <div
+        class="w-full py-2 text-center align-middle font-semibold text-sm text-white rounded bg-white bg-opacity-10 hover:bg-opacity-20">
+        Consulting
+      </div>
+
+    </NuxtLink>
+  </nav>
+</template>
